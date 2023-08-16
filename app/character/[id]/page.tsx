b4ec958221542e3ec4e014a5968437185526eb82
mongarerobert3 <mongarerobert3@gmail.com>
2023-08-16 22:54:58 +0300

    Updated documentation

--- app/character/[id]/page.tsx
+++ app/character/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import useFetch from '@app/api/useFetch';
@@ -16,9 +15,6 @@ const CharacterPage = () => {
   const id = pathname.split('/').pop();
   const { data, error } = useFetch(id);
   
-  /**if (isLoading) {
-    return <Loading />;
-  }**/
 
   if (error) {
     return <div className="text-center py-8">Error: {error}</div>;
@@ -29,31 +25,48 @@ const CharacterPage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       {character ? (
-        <div className="flex max-w-3xl p-8 bg-white rounded shadow">
-          <div className="flex-shrink-0">
+        <div className="max-w-3xl p-8 bg-white rounded shadow">
+          <div className="md:flex justify-center">
             <img
               src={character.image}
               alt={character.name}
               className="w-48 h-auto rounded-lg"
             />
           </div>
-          <div className="ml-8">
+          <div className="text-center md:text-left md:mt-0 mt-4">
             <h1 className="char-name text-2xl font-bold mb-2">{character.name}</h1>
             {character.alternate_names && character.alternate_names.length > 0 && (
-              <p className="char-alt-name text-gray-600">
-              Alternate Names: {character.alternate_names.join(", ")}
-            </p>
+              <p className="char-details char-alt-name text-gray-600">
+                <span className="char-label-background">Alternate Names</span> {character.alternate_names.join(", ")}
+              </p>
             )}
-            <p className="char-dob text-gray-600">D.O.B: {character.dateOfBirth}</p>
-            <p className="char-yob text-gray-600">Y.O.B: {character.yearOfBirth}</p>
-            <p className="char-wizard text-gray-600">Wizard:{character.wizard.toString()}</p>
-            <p className="char-species text-gray-600">Species: {character.species}</p>
-            <p className="char-gender text-gray-600">Gender: {character.gender}</p>
-            <p className="char-house text-gray-600">House: {character.house}</p>
-            <p className="char-ancestry text-gray-600">Ancestry: {character.ancestry}</p>
-            <p className="char-eyeColour text-gray-600">Eye Colour: {character.eyeColour}</p>
-            <p className="char-hairColour text-gray-600">Hair Colour: {character.hairColour}</p>
-
+            <p className="char-details char-dob text-gray-600">
+              <span className="char-label-background">D.O.B</span> {character.dateOfBirth}
+            </p>
+            <p className="char-details char-yob text-gray-600">
+              <span className="char-label-background">Y.O.B</span> {character.yearOfBirth}
+            </p>
+            <p className="char-details char-wizard text-gray-600">
+              <span className="char-label-background">Wizard</span> {character.wizard.toString()}
+            </p>
+            <p className="char-details char-species text-gray-600">
+              <span className="char-label-background">Species</span> {character.species}
+            </p>
+            <p className="char-details char-gender text-gray-600">
+              <span className="char-label-background">Gender:</span> {character.gender}
+            </p>
+            <p className="char-details char-house text-gray-600">
+              <span className="char-label-background">House</span> {character.house}
+            </p>
+            <p className="char-details char-ancestry text-gray-600">
+              <span className="char-label-background">Ancestry</span> {character.ancestry}
+            </p>
+            <p className="char-details char-eyeColour text-gray-600">
+              <span className="char-label-background">Eye Colour</span> {character.eyeColour}
+            </p>
+            <p className="char-details char-hairColour text-gray-600">
+              <span className="char-label-background">Hair Colour</span> {character.hairColour}
+            </p>
           </div>
         </div>
       ) : (
